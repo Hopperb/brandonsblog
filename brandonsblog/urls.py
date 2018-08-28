cb81266ea721736e69ffcb76013d59c86512fcf6
Brandon Hopper <brandonhopper0@gmail.com>
2018-08-28 10:25:45 -0400

    -added route for static files to render pictures in root route -rendering blogs in root route but pics are not displaying yet

--- brandonsblog/urls.py
+++ brandonsblog/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url
 from posts import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url('admin/', admin.site.urls),
     url(r'$^', views.home)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
