f873f0de4d2410b267436ad25ff92f32c511cc6b
Brandon Hopper <brandonhopper0@gmail.com>
2018-08-28 00:40:41 -0400

    -modified model to be able to upload images -modifend redner method for post to loop throuh and list logs -implemented string method to label posts in db

--- posts/models.py
+++ posts/models.py
@@ -5,10 +5,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    title = models.CharField(max_length=200)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField()
     last_modified = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='media/', blank=True, null=True)
+    content = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.title
+   
 
 class PersonalPost(Post):   # Inherits from Note!
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'posts/home.html')
+    
+    posts = Post.objects.order_by('created_at')
+
+    return render(request, 'posts/home.html', {'posts':posts})
